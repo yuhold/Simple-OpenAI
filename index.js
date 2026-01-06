7231eeaa576840de1b6e6f66018c746159be722a
yuhold <yuhold.0757@qq.com>
2026-01-06 19:32:21 +0800

    修复了自定义插件名字时候导致的报错问题

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import fs from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'url'
 
-// 读取 apps 目录下的所有 js 文件
-const files = fs.readdirSync('./plugins/Simple-OpenAI/apps').filter(file => file.endsWith('.js'))
+// 1. 动态获取当前文件的绝对路径
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// 2. 拼接 apps 目录的路径 (无论插件文件夹叫什么，都能找到 apps)
+const appsDir = path.join(__dirname, 'apps')
+
+// 读取 apps 下的文件
+const files = fs.readdirSync(appsDir).filter(file => file.endsWith('.js'))
 
 let ret = []
 
 files.forEach((file) => {
-    ret.push(import(`./apps/${file}`))
+    // 3. 使用绝对路径 import
+    ret.push(import(`file://${path.join(appsDir, file)}`))
 })
 
 ret = await Promise.all(ret)
@@ -15,12 +25,9 @@ let apps = {}
 for (let i in files) {
     let name = files[i].replace('.js', '')
 
-    // 适配 export class 写法
     if (ret[i].default) {
-        // 如果是 export default class
         apps[name] = ret[i].default
     } else {
-        // 如果是 export class (我们用的是这种)
         Object.keys(ret[i]).forEach(key => {
             apps[key] = ret[i][key]
         })
